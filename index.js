fdd9f330233baed47e6c0f724cf446fcedd1ddc5
JSanti99 <jeisson.santiesteban@uptc.edu.co>
2020-08-19 21:03:00 -0500

    :sparkles: Middlewares para manejo de errores, 404 y validacion de esquema agregados

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const app = express();
 const port = 3000;
 //API
 const songsAPI = require('./routes/songs');
+//middleware
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler,
+} = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
 
 //routes
 songsAPI(app);
+
+// catch 404
+app.use(notFoundHandler);
+
+//middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
