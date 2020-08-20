0e9e90a3abe81a1bb403910ec20c290f7c2ff8be
cristianfonseca03 <cristian.lfs@gmail.com>
2020-08-19 21:25:38 -0500

    :sparkles: Esquema de canciones creado

---
+++ utils/schemas/songs.js
@@ -0,0 +1,36 @@
+const joi = require('@hapi/joi');
+
+const songIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const songAlbumTitleSchema = joi.string().max(250);
+const songTitleSchema = joi.string().max(250);
+const songAuthorsSchema = joi.array().items(joi.string().max(250)).min(1);
+const songCompositorsSchema = joi.array().items(joi.string().max(250)).min(1);
+const songYearSchema = joi.integer().min(1880);
+const songDurationSchema = joi.integer().min(1);
+const songGendersSchema = joi.array().items(joi.string().max(250)).min(1);
+
+const createSongSchema = {
+  albumTitle: songAlbumTitleSchema.required(),
+  title: songTitleSchema.required(),
+  authors: songAuthorsSchema.required(),
+  compositors: songCompositorsSchema.required(),
+  year: songYearSchema.required(),
+  duration: songDurationSchema.required(),
+  genders: songGendersSchema.required(),
+};
+
+const updateSongSchema = {
+  albumTitle: songAlbumTitleSchema,
+  title: songTitleSchema,
+  authors: songAuthorsSchema,
+  compositors: songCompositorsSchema,
+  year: songYearSchema,
+  duration: songDurationSchema,
+  genders: songGendersSchema,
+};
+
+module.exports = {
+  songIdSchema,
+  createSongSchema,
+  updateSongSchema,
+};
